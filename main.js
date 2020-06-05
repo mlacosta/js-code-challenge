b7a38eedfbb3147471de049f1c19a5962e206836
mlacosta <marianoacosta.003@gmail.com>
2020-06-05 00:30:30 -0300

    editing the mutate function

--- main.js
+++ main.js
@@ -15,8 +15,33 @@ const mockUpStrand = () => {
 
 
 
+const pAquorFactory = (number,dnaBase) =>{
+    return {
+        _specimenNum:number,
+        _dna: dnaBase,
+        get dna(){
+            return this._dna;
+        },
+        set dna(dnaIn){
+            this._dna = dnaIn;
+        }
+        mutate(){
+            const dnaBases = ['A', 'T', 'C', 'G'];
+            const len = this.dna.length;
+            const randInx = Math.floor(Math.random() * len);
+            const base = this.dna[randInx]; 
+            const mutatedBase = dnaBases.filter(b => b !== base);
+            console.log('selected base: '+base)
+            console.log('remaining bases: '+mutatedBase);
+            const ind = Math.floor(Math.random() * 3);
+            this.dna = 
+
+        }
+    }
+
+}
 
 
+const specimen = pAquorFactory(1524,mockUpStrand());
 
-
-
+specimen.mutate()
